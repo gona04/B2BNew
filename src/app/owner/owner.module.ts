7218e9514b423177d58036495179ef50dc0eb9a0
Goonja Gandhi <gandhi.goonja@gmail.com>
2018-12-05 19:11:55 +0530

    Deleted the general folder and made the relative changes

--- src/app/owner/owner.module.ts
+++ src/app/owner/owner.module.ts
@@ -4,7 +4,6 @@ import { AddEngineersComponent } from './add-engineer/add-engineers.component';
 import { EngineerDetailsComponent } from './engineer-details/engineer-details.component';
 import { AddTechnologyComponent } from './add-engineer/add-technology/add-technology.component';
 import { AddJobsComponent } from './add-engineer/add-jobs/add-jobs.component';
-import { EngineerListGeneralComponent } from './engineer-list-general/engineer-list-general.component';
 import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
 import { AddEmployeeComponent } from './employees/add-employee/add-employee.component';
 import { DeletedEmployeesListComponent } from './employees/deleted-employees-list/deleted-employees-list.component';
@@ -12,7 +11,6 @@ import { DeletedEngineersComponent } from './deleted-engineers/deleted-engineers
 import { FilterPipe } from "../shared/filter.pipe";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
-import { AuthRoutingModule } from "../authentication/auth.routing-module";
 import { CustomMaterialModule } from "../shared/custom-material/custom-material.module";
 import { CustomAngularMaterialModule } from "../shared/custom-material/custom-angular-bootstrap.module";
 import { CustomPrimeNgModule } from "../shared/custom-material/custom-primeNg.module";
@@ -26,7 +24,6 @@ import { OwnerRouting } from "./owner-routing.module";
         DeletedEngineersComponent,
         AddTechnologyComponent,
         AddJobsComponent,
-        EngineerListGeneralComponent,
         FilterPipe,
         EmployeeListComponent,
         AddEmployeeComponent,
